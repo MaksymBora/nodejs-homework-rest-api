c28caf213185e513fa3caeab021d8c50e0bb9f6d
MaksymBora <maxboraod@gmail.com>
2023-11-03 22:18:15 +0100

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
     es2021: true,
   },
   extends: 'eslint:recommended',
-  plugin: 'node/recommended',
   overrides: [
     {
       env: {
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,65 @@
-// const fs = require('fs/promises')
+import { promises as fs } from 'fs';
+import { nanoid } from 'nanoid';
+import path from 'path';
+import 'colors';
 
-const listContacts = async () => {}
+const contactsPath = path.resolve('./db/contacts.json');
 
-const getContactById = async (contactId) => {}
+export async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
 
-const removeContact = async (contactId) => {}
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error.red);
+  }
+}
 
-const addContact = async (body) => {}
+export async function getContactById(contactId) {
+  try {
+    const data = await listContacts();
+    return data.find(contact => contact.id === contactId) || null;
+  } catch (error) {
+    console.log(error.red);
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+export async function removeContact(contactId) {
+  try {
+    const data = await listContacts();
+    const UpdatedContacts = data.filter(({ id }) => id !== contactId);
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(UpdatedContacts, null, 2),
+      'utf-8',
+    );
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+    const deletedContact =
+      data.find(contact => contactId === contact.id) || null;
+
+    return deletedContact;
+  } catch (error) {
+    console.log(error.red);
+  }
+}
+
+export async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    const updatedContacts = [newContact, ...contacts];
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2), {
+      encoding: 'utf-8',
+    });
+
+    return newContact;
+  } catch (error) {
+    console.log(error.red);
+  }
 }

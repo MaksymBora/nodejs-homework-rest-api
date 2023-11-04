16dcffc59c28913a66f768ed0a7948c7eb88ee67
MaksymBora <maxboraod@gmail.com>
2023-11-04 22:14:32 +0100

    update

--- app.js
+++ app.js
@@ -28,15 +28,3 @@ app.use((err, _, res, __) => {
 });
 
 export default app;
-
-// const contactSchema = joi.object({
-//   name: joi.string().min(3), //NAME SHOULD HAVE MINIMUM 3 SYMBOLS
-//   email: joi.string().email(), //EMAIL SHOULD BE IN CORRECT FORMAT
-//   phone: joi.string().min(5), //PHONE SHOULD HAVE AT LEAST 5 SYMBOLS
-// });
-
-// const validator = schema => body => {
-//   return schema.validate(body);
-// };
-
-// const contactValidator = validator(contactSchema);
